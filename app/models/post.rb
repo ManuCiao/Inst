e3185381838e658dcc43dd21152aa46943f50bfb
ManuCiao <mn.sabatino@gmail.com>
2016-11-27 19:03:45 +0000

    Fix the Paperclip Errno::EACCES (Permission denied - /system) issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,9 @@ class Post < ActiveRecord::Base
     medium: '200x200>',
     large: '300x300>'
   },
-  :default_url => "/images/:style/missing.png"
+  :default_url => "/images/:style/missing.png",
+  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+  :url => "/system/:attachment/:id/:style/:filename"
 
   validates :image,
     attachment_content_type: {content_type: /\Aimage\/.*\Z/},
